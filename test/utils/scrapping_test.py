a6f3513ca8baa48b6ce2f44b54c05d55e46f8723
alexandre-assad <alexandre.assad@laplateforme.io>
2023-10-19 09:12:03 +0200

    [ADD] scrap number ants method

--- test/utils/scrapping_test.py
+++ test/utils/scrapping_test.py
@@ -3,8 +3,8 @@ from utils.scrapping import scrap_rooms, scrap_number_ants
 
 def test_scrap_number_ants():
     assert scrap_number_ants("fourmiliere_trois.txt") == 5
-    assert scrap_number_ants("fourmiliere_cinq.txt") == 5
+    assert scrap_number_ants("fourmiliere_cinq.txt") == 50
 
 def test_scrap_rooms():
-    assert scrap_rooms("fourmiliere_deux.txt") == {"Sv":[["S1"], None],"S1":[["S2"],None],"S2":[["Sd"],None],"Sd":[["Sv"],None]}
-    assert scrap_rooms("fourmiliere_quatre.txt") == {"Sv":[["S1"],None],"S1":[["S2","S3"],2],"S2":[["S4"],None],"S3":[["S4"],None],"S4":[["S5","S6"],2],"S5":[["Sd"],None],"S6":[["Sd"],None]}
+    assert scrap_rooms("fourmiliere_deux.txt") == {"Sv":[["S1"], None],"S1":[["S2","Sv"],None],"S2":[["Sd","S1"],None],"Sd":[["Sv","S2"],None]}
+    assert scrap_rooms("fourmiliere_quatre.txt") == {"Sv":[["S1"],None],"S1":[["S2","S3","SV"],2],"S2":[["S4","S1"],None],"S3":[["S4","S1"],None],"S4":[["S2","S3","S5","S6"],2],"S5":[["Sd","S4"],None],"S6":[["Sd","S4"],None],"Sd":[["S5","S6"],None]}
--- utils/scrapping.py
+++ utils/scrapping.py
@@ -1,6 +1,13 @@
+import os
+import re
 
 def scrap_number_ants(file:str) -> int:
-    pass
+    #Get the content in string format
+    f = open(os.path.join("..","anthills",file), "r")
+    content = f.read()
+    regex_exp = r"f=(.*)\n"
+    regex_str = re.search(regex_exp,content)
+    return int(regex_str.group(1))
 
 def scrap_rooms(file:str) -> list:
-    pass
+    return []
