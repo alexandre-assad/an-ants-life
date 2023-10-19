ff7f8be0763a38dde13d627fad5a15a98471e739
alexandre-assad <alexandre.assad@laplateforme.io>
2023-10-19 13:25:38 +0200

    [ADD] Test anthill get_direction_room

--- anthill.py
+++ anthill.py
@@ -3,9 +3,17 @@ from utils.scrapping import scrap_number_ants, scrap_rooms
 class Anthill:
 
     def __init__(self,anthill_file:str):
-        self.n_ants = scrap_number_ants(anthill_file)
-        self.rooms = scrap_rooms(anthill_file)
+        # Set up the anthill
+        self.n_ants: int = scrap_number_ants(anthill_file)
+        self.rooms: dict = scrap_rooms(anthill_file)   #FORMAT : self.rooms["ROOM"] = [[room_linked_1,room_linked_2],capacity,ant_in_room]
+        self.rooms["Sd"][1] = self.n_ants
+
         # Set up all ants
         for key,values in self.rooms.items():
             self.rooms[key].append(0)
         self.rooms["Sv"][2] = self.n_ants
+
+
+    def get_direction_room(self):
+        #We want to have a direction, so we will think about "level", the distance from a room to the dormitory
+        pass
--- test/anthill_test.py
+++ test/anthill_test.py
@@ -5,3 +5,9 @@ def test_init():
     anthill = Anthill("fourmiliere_quatre.txt")
     assert anthill.n_ants==10
     assert anthill.rooms["Sv"] == [["S1"],1,10]
+    assert anthill.rooms["Sd"] == [["S5","S6"],10,0]
+
+
+def test_get_direction_room():
+    anthill = Anthill("fourmiliere_deux.txt")
+    assert anthill.rooms == {"Sv":[["S1","Sd"], 1],"S1":[["S2"],1],"S2":[["Sd"],1],"Sd":[[],1]}
